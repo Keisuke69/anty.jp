44e449198c23cea8c6be236abdad9ac6f00a0fc4
Keisuke69 <providence000@gmail.com>
2021-05-19 00:49:53 +0000

    Adjust layout slightly

--- pages/index.js
+++ pages/index.js
@@ -13,16 +13,23 @@ export default function Home() {
 
       <main className={styles.main}>
         {/* <h1 className={styles.title}>Anty</h1> */}
-
+        <p>
+          <font color="white">以下のサイトで購入できます</font>
+        </p>
         <div className={styles.grid}>
           <a href="https://minne.com/@anty-000" target="_blank">
             <h2>minne</h2>
           </a>
-        </div>
-        <div>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <a href="https://www.creema.jp/c/anty" target="_blank">
             <h2>Creema</h2>
           </a>
+        </div>
+
+        <div style={{ marginTop: "100px" }}>
+          <Link href="/about">
+            <a>会社概要</a>
+          </Link>
         </div>
       </main>
 
